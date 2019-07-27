d0af226ed7ee47d72d0ffebf171f9d6975c7f146
Eliza Munroe <era3munroe@gmail.com>
2019-07-27 23:42:05 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   new_array = [ ]
    index = 0
     while index < array.length
-      new_array << array
+      new_array << array.upcase
       yield array[index]
      index += 1 
     end
