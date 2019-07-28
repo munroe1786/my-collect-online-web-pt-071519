0f6f3eae1d307e3d50799b498bf49437e62b8428
Eliza Munroe <era3munroe@gmail.com>
2019-07-28 00:14:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+  new_array = [ ]
+   index = 0
+    while index < array.length 
+      new_array.push yield array[index]
+     index += 1 
+    end
+    return new_array
+end
 
-
+  
+  
