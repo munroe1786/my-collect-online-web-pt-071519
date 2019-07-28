3550424cf77babe5460564180918c68ef6f918c6
Eliza Munroe <era3munroe@gmail.com>
2019-07-28 00:12:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect(array)
   new_array = [ ]
    counter = 0
-    while counter < array.length do 
+    while counter < array.length 
       yield array[counter]
      counter += 1 
     end
