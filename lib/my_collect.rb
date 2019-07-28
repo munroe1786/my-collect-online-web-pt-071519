1bcfa0b5608aea8d74bfc64d4da2f560a3c22e64
Eliza Munroe <era3munroe@gmail.com>
2019-07-28 00:13:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(array)
   new_array = [ ]
-   counter = 0
-    while counter < array.length 
-      yield array[counter]
-     counter += 1 
+   index = 0
+    while index < array.length 
+      yield array[index]
+     index += 1 
     end
     return new_array
 end
