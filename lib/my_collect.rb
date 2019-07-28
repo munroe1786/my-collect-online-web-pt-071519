41eab6eaeb7a00216b4d0ba40084d44caca23426
Eliza Munroe <era3munroe@gmail.com>
2019-07-28 00:07:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(array)
   new_array = [ ]
    index = 0
-    while index < array.length do |language|
-      new_array << language.upcase
+    while index < array.length do |name|
+      new_array << name
       yield array[index]
      index += 1 
     end
